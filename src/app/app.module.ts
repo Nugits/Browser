f93a796f02ab37b7c7a965d77adc66a4f6e20643
Yuriy Kuchevskiy <yurakuchevskiy@mail.ru>
2017-11-04 17:43:06 +0200

    Added base http service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { IndexComponent } from './components/index/index.component';
 import { RepositoryComponent } from './components/repository/repository.component';
+
+import { HttpService } from './services/http.service';
 
 @NgModule({
   declarations: [
@@ -18,10 +21,11 @@ import { RepositoryComponent } from './components/repository/repository.componen
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/repository/repository.component.ts
+++ src/app/components/repository/repository.component.ts
@@ -1,22 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
 import { ActivatedRoute, Router, Route } from '@angular/router';
+
+import { HttpService } from '../../services/http.service';
 
 @Component({
   selector: 'app-repository',
   templateUrl: './repository.component.html',
-  styleUrls: ['./repository.component.css']
+  styleUrls: ['./repository.component.css'],
+  providers: [HttpService]
 })
 export class RepositoryComponent implements OnInit {
 
   constructor(
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private httpService: HttpService
   ) { }
 
   ngOnInit() {
     let owner: string = this.activatedRoute.snapshot.params['owner'];
     let repo: string = this.activatedRoute.snapshot.params['repository'];
     console.log(`Owner: ${owner}, Repo: ${repo}`);
+    this.httpService.getRequest<string>('test')
+      .then(res => console.log(res));
   }
 
 }
